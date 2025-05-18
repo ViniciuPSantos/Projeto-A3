513398701a15fccad22dd8300a710250baa55989
Vinicius <vipaulasantos@gmail.com>
2025-05-18 12:29:26 -0300

    Update DBConnector.java

--- projetoa3/src/db/DBConnector.java
+++ projetoa3/src/db/DBConnector.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnector {
     private static final String URL = "jdbc:mysql://localhost:3306/banco_projetoa3";
     private static final String USER = "root"; 
-    private static final String PASSWORD = "vini280706_@Vp";
+    private static final String PASSWORD = "senha"; // Substitua pela sua senha
 
     public static Connection conectar() throws SQLException{
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/banco_projetoa3", "root", "vini280706_@Vp");
